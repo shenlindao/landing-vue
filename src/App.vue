9d677be190d75022157bf58a16e94dbffaaf5084
shenlin <shenlin@yandex.ru>
2021-08-16 01:33:26 +0300

    Add logo component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="items">
+      <ItemCard
+        v-for="item in items"
+        :itemName="item.name"
+        :itemColors="item.colors"
+        :key="item.id"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import ItemCard from './components/items/ItemCard.vue';
+
+const ITEM_COLORS = [
+  { name: 'Красный', color: 'red' },
+  { name: 'Зеленый', color: 'green' },
+  { name: 'Голубой', color: 'blue' },
+  { name: 'Черный', color: 'black' },
+];
+const ITEMS = [
+  { id: 1, name: 'Крусло 1', colors: ITEM_COLORS },
+  { id: 2, name: 'Крусло 2', colors: ITEM_COLORS },
+  { id: 3, name: 'Крусло 3', colors: ITEM_COLORS },
+  { id: 4, name: 'Крусло 4', colors: ITEM_COLORS },
+];
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    ItemCard,
+  },
+  data() {
+    return {
+      items: ITEMS,
+    };
   },
 };
 </script>
