6d523561bc0727707a96d8f4e1bc051a392c2787
shenlin <shenlin@yandex.ru>
2021-08-16 01:30:35 +0300

    Add simple button component

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-on:click="$emit('buttonClick')" class="button">
+    {{buttonText}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItemCard',
+  props: {
+    buttonText: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .button {
+    width: 150px;
+    height: 50px;
+    background-color: rgb(177, 53, 53);
+    color: rgb(231, 231, 231);
+    &:hover {
+      cursor: pointer;
+      box-shadow: 0px 4px 10px rgb(0 0 0 / 25%);
+    }
+  }
+</style>
